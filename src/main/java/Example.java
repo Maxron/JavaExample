e2156c096aa67338ae299c63c4b96ab85630f770
Wei <zero.maxima@gmail.com>
2019-03-26 22:52:25 +0800

    [Add] Login by email

---
+++ src/main/java/Example.java
@@ -0,0 +1,8 @@
+public class Example {
+    public static void main(String[] args) {
+        LoginView loginView = new LoginView();
+        loginView.login("test@gmailcom", "passwd");
+
+    }
+
+}
---
+++ src/main/java/LoginView.java
@@ -0,0 +1,5 @@
+public class LoginView {
+    public void login(String email, String passwd) {
+        System.out.println("Login :" + email);
+    }
+}
