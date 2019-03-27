45b88313e156e51b609c8feed81c8e9822c3e019
Wei <zero.maxima@gmail.com>
2019-03-27 09:52:51 +0800

    [Update] Modify parameter name

--- src/main/java/LoginView.java
+++ src/main/java/LoginView.java
@@ -1,8 +1,8 @@
 public class LoginView {
     private LoginService loginService;
 
-    public LoginView(LoginService emailLoginService) {
-        this.loginService = emailLoginService;
+    public LoginView(LoginService loginService) {
+        this.loginService = loginService;
     }
 
     public void startLogin() {
